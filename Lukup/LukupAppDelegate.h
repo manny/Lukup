085e2a7716939850b7f31d296200c06117fe4858
Carlos Folgar <folgar.tech@gmail.com>
2013-08-02 23:14:52 -0700

    Initial Commit

---
+++ Lukup/LukupAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LukupAppDelegate.h
+//  Lukup
+//
+//  Created by Carlos Folgar on 8/2/13.
+//  Copyright (c) 2013 CODE2040. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LukupAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) IBOutlet UIWindow *window;
+
+@end
---
+++ Lukup/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lukup
+//
+//  Created by Carlos Folgar on 8/2/13.
+//  Copyright (c) 2013 CODE2040. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, nil);
+    }
+}
